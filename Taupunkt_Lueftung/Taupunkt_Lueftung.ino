db5faf5a9f335487a5645d0baa2b50e3ff410bf0
Robert Feld <robertmfeld@gmail.com>
2022-12-23 00:20:12 +0100

    Korrekturwerte auf 0 gesetzt

--- Taupunkt_Lueftung/Taupunkt_Lueftung.ino
+++ Taupunkt_Lueftung/Taupunkt_Lueftung.ino
@@ -17,10 +17,10 @@ bool rel;
 #define DHTTYPE_2 DHT22 // DHT 22  
 
 // *******  Korrekturwerte der einzelnen Sensorwerte  *******
-#define Korrektur_t_1  0.2 // Korrekturwert Innensensor Temperatur
-#define Korrektur_t_2  0.8 // Korrekturwert Außensensor Temperatur
-#define Korrektur_h_1  -1.8  // Korrekturwert Innensensor Luftfeuchtigkeit
-#define Korrektur_h_2  1.4  // Korrekturwert Außensensor Luftfeuchtigkeit
+#define Korrektur_t_1  0 // Korrekturwert Innensensor Temperatur
+#define Korrektur_t_2  0 // Korrekturwert Außensensor Temperatur
+#define Korrektur_h_1  0  // Korrekturwert Innensensor Luftfeuchtigkeit
+#define Korrektur_h_2  0  // Korrekturwert Außensensor Luftfeuchtigkeit
 //***********************************************************
 
 #define SCHALTmin   5.0 // minimaler Taupunktunterschied, bei dem das Relais schaltet
